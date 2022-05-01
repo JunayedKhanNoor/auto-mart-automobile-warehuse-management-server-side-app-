357e2a335f03fd879ef45af2b918e8c87c2e52f5
163002029.junayed <junayednoor@gmail.com>
2022-05-01 15:59:50 +0600

    Find One API

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -31,6 +31,13 @@ async function run(){
                 vehicles = await cursor.toArray();
             }
             res.send(vehicles);
+        })
+        //Find One
+        app.get('/inventory/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)}
+            const inventory = await automobileCollection.findOne(query);
+            res.send(inventory);
         })
         
         //Post Items
